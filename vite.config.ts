1017db4a2ad7482ec185a437b0fb5e9b55b2d114
Malin <maa.lin@outlook.com>
2025-06-05 11:51:36 +0200

    Update base path for Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "Todo-list",
+  base: "/Todo-list/",
   plugins: [react(), tailwindcss()],
 })
